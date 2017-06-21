e40a58ddc1a72d0c9778239a355a92e50a41dda9
root <root@4cc4dd807910>
2017-06-21 10:43:34 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_RxnAjubipV",
+  "homePage" : "Main",
+  "name" : "Auto_RxnAjubipV",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
